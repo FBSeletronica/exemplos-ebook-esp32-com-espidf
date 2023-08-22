f74448708c8a70c60ab78be6e21f6fdc5570eaf1
Fábio Souza <fs.embarcados@gmail.com>
2023-08-22 20:48:57 -0300

    Ajuste dos comentários

--- hello_world/main/hello_world.c
+++ hello_world/main/hello_world.c
@@ -3,23 +3,22 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
+// Função principal do programa
 void app_main(void)
 {
     // Configuração do pino GPIO para o LED (número do pino pode variar)
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
     
-    printf("Hello World! \n");
+    printf("Hello World! \n");  // Mensagem de inicialização
 
-    while (1)
+    while (1)                                  // Loop infinito
     {
-        // Liga o LED
-        gpio_set_level(GPIO_NUM_2, 1);
+        gpio_set_level(GPIO_NUM_2, 1);         // Liga o LED
         vTaskDelay(1000 / portTICK_PERIOD_MS); // Aguarda 1 segundo
-        printf("LED - ON \n");
+        printf("LED - ON \n");                 // Mensagem de LED ligado
 
-        // Desliga o LED
-        gpio_set_level(GPIO_NUM_2, 0);
+        gpio_set_level(GPIO_NUM_2, 0);         // Desliga o LED
         vTaskDelay(1000 / portTICK_PERIOD_MS); // Aguarda 1 segundo
-        printf("LED - OFF \n");
+        printf("LED - OFF \n");                // Mensagem de LED desligado
     }
 }
